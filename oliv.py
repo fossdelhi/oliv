e1e1dcec52c9d51932c019151fd41ca797c6cdba
inquiridortechie <narya436.na@gmail.com>
2019-02-18 05:20:31 +0530

    feat(oliv.py): Add functions
    * feat(oliv.py): Add pickup_dotconfig()
    
    * feat(oliv.py): Add search_config_files()

--- oliv.py
+++ oliv.py
@@ -1,12 +1,33 @@
 import click
 import subprocess
 import os
+import shutil
 
 
-def pickup_config_files():
+def pickup_dotconfig():
     user = os.path.expanduser('~')
-    list = ['.bashrc', '.zshrc', '.vimrc', '.profile', '.gitconfig']
-    for file in list:
+    source = user + '/.config'
+    destination = './dotfile/.config/'
+    shutil.copytree(source, destination)
+
+
+def search_config_files():
+    list = ['.aliases', '.bashrc', '.cookiecutterrc',
+            '.exports', '.extra', '.functions', '.gdbinit',
+            '.gitattributes', '.gitconfig', '.gitignore',
+            '.gitmessage', '.inputrc', '.linux', '.osx',
+            '.profile', '.travis.yml', '.vimrc', '.wgetrc',
+            '.zshrc']
+    files = []
+    for x in list:
+        if (os.path.exists(os.path.expanduser('~') + '/' + x)):
+            files.append(x)
+    pickup_config_files(files)
+
+
+def pickup_config_files(files):
+    user = os.path.expanduser('~')
+    for file in files:
         if (os.path.exists(user + '/' + file)):
             copy = user + '/' + file
             destination = './dotfile/' + file
@@ -38,15 +59,17 @@ def system_setup():
 
 
 @click.command()
-@click.option('--make', '-m', nargs=2, type=str,
+@click.option('--make', '-m', is_flag=True,
               help="To make your oliv_dotfile setup")
-@click.option('--setup', '-s', nargs=2, type=str,
+@click.option('--setup', '-s', is_flag=True,
               help="To configure your system with your oliv_dotfile setup")
 def oliv(make, setup):
     if make:
         subprocess.call(['mkdir', './dotfile'])
-        pickup_config_files()
+        pickup_dotconfig()
+        search_config_files()
         pickup_python_packages()
         pickup_installed_packages()
     elif setup and os.path.isdir('./oliv_dotfile'):
         system_setup()
+
